ef33614d15da391870a9d3739b4cf06e6ded0945
gamefreak20 <joeystil3@gmail.com>
2021-05-06 19:28:06 +0100

    database connection finished

--- bot/services/database/tables/index.js
+++ bot/services/database/tables/index.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 
 
-const modules = fs.readdirSync(__dirname).filter(fileName => fileName !== 'index.js').map(module => module.split('.js')[0]);
+const modules = fs.readdirSync(__dirname).filter(module => !module.endsWith(".js"));
 
 modules.forEach(moduleName => {
-    module.exports[moduleName] = require('./' + moduleName).run;
+    module.exports[moduleName] = require('./' + moduleName);
 });
--- bot/services/settings/getCommandPrefix.js
+++ bot/services/settings/getCommandPrefix.js
@@ -1,5 +1,5 @@
 module.exports.run = (serverId, services, callback) => {
-    services.database.tables.server(serverId, services,  (error, result) => {
+    services.database.tables.server.get(serverId, services,  (error, result) => {
         callback(result[0].commandPrefix);
     });
 }
