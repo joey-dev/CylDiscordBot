d5508f8d99ba6f36fed6fb355e570a320562be68
gamefreak20 <joeystil3@gmail.com>
2022-01-28 00:13:48 +0100

    added starting part for adding the bot to the database on joining a server

---
+++ bot/events/guildCreate.js
@@ -0,0 +1,11 @@
+module.exports.run = async (client, services, guild) => {
+    // services.settings.getLanguage(guild.id, services, (language => {
+    //
+    // }));
+
+    addBotToDatabase(client, services, guild)
+};
+
+function addBotToDatabase(client, services, guild) {
+    //TODO: add bot to database
+}
