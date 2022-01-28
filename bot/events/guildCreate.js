1f615fa5f9cb83c5b0d123c359d468b5bc1babba
gamefreak20 <joeystil3@gmail.com>
2022-01-28 20:38:35 +0100

    added message on bot join with link for dashboard

--- bot/events/guildCreate.js
+++ bot/events/guildCreate.js
@@ -3,9 +3,32 @@ module.exports.run = async (client, services, guild) => {
     //
     // }));
 
-    addBotToDatabase(client, services, guild)
+    addBotToDatabase(client, services, guild);
+    sendMessageToMainOrFirstChannelOnJoin(guild);
 };
 
 function addBotToDatabase(client, services, guild) {
     services.database.tables.server.post(guild.id, guild.name, services, () => {})
+}
+
+function sendMessageToMainOrFirstChannelOnJoin(guild) {
+    const dashboardUrl = "http://localhost:3000/dashboard/";
+    const messageToSend = "Hello, thanks for inviting me! \nTo get the most out of this bot, configure it here: " + dashboardUrl + guild.id;
+
+    if (guild.systemChannelID) {
+        let channel = guild.channels.cache.get(guild.systemChannelID);
+        channel.send(messageToSend);
+    } else {
+        let channelID;
+        let channels = guild.channels.cache;
+
+        channels.each(channel => {
+            if (!channelID && channel.type === "GUILD_TEXT" && channel.permissionsFor(guild.me).has(`SEND_MESSAGES`)) {
+                channelID = channel.id;
+            }
+        });
+
+        let channel = guild.channels.cache.get(channelID);
+        channel.send(messageToSend);
+    }
 }
